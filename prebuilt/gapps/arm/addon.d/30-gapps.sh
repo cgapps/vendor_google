d530a65b715d4634433ac649ac0f0e558ef7bd44
jrizzoli <joey@cyanogenmoditalia.it>
2015-06-11 17:24:59 +0200

    15.07 Update:
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- prebuilt/gapps/arm/addon.d/30-gapps.sh
+++ prebuilt/gapps/arm/addon.d/30-gapps.sh
@@ -29,7 +29,7 @@ priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk
 priv-app/GooglePartnerSetup/GooglePartnerSetup.apk
 priv-app/GoogleServicesFramework/GoogleServicesFramework.apk
 priv-app/Phonesky/Phonesky.apk
-priv-app/PrebuiltGmsCore/PreBuiltGmsCore.apk
+priv-app/PrebuiltGmsCore/PrebuiltGmsCore.apk
 priv-app/PrebuiltGmsCore/lib/arm/libAppDataSearch.so
 priv-app/PrebuiltGmsCore/lib/arm/libconscrypt_gmscore_jni.so
 priv-app/PrebuiltGmsCore/lib/arm/libgames_rtmp_jni.so
