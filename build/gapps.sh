ae166a10ad8c683182a1cd437242520254e40684
Peter Hyman <pete@peterhyman.com>
2015-06-21 08:43:25 -0400

    add md5sum, some cleanup.

--- build/gapps.sh
+++ build/gapps.sh
@@ -28,6 +28,7 @@ function create(){
     PREBUILT=$TOP/prebuilt/gapps/$GARCH
     if [ -d $OUT/$GARCH ]; then
         echo "Previous build found for $GARCH!"
+        return 1
     else
         echo "No previous build found for $GARCH!"
         mkdir $OUT
@@ -37,30 +38,31 @@ function create(){
     cp -r $PREBUILT $OUT/$GARCH
     mv $OUT/$GARCH/$GARCH $OUT/$GARCH/arch
     cp -r $COMMON $OUT/$GARCH
+    return 0
 }
 
 function zipit(){
-    if [ "$LASTRETURN" == 0 ]; then
-        BUILDZIP=gapps-$ANDROIDV-$DATE.zip
-        echo "Importing installation scripts..."
-        cp -r $METAINF $OUT/$GARCH/META-INF
-        echo "Creating package..."
-        cd $OUT/$GARCH
-        zip -r /tmp/$BUILDZIP . &>/dev/null
-        rm -rf $OUT/tmp
-        cd $TOP
-        if [ -f /tmp/$BUILDZIP ]; then
-            echo "Signing zip..."
-            java -Xmx2048m -jar $TOP/build/sign/signapk.jar -w $TOP/build/sign/testkey.x509.pem $TOP/build/sign/testkey.pk8 /tmp/$BUILDZIP $OUT/$GARCH/$BUILDZIP
-        else
-            printerr "Couldn't zip files!"
-            return 1
+    BUILDZIP=gapps-$ANDROIDV-$DATE.zip
+    echo "Importing installation scripts..."
+    cp -r $METAINF $OUT/$GARCH/META-INF
+    echo "Creating package..."
+    cd $OUT/$GARCH
+    zip -r /tmp/$BUILDZIP . &>/dev/null
+    rm -rf $OUT/tmp
+    cd $TOP
+    if [ -f /tmp/$BUILDZIP ]; then
+        echo "Signing zip..."
+        java -Xmx2048m -jar $TOP/build/sign/signapk.jar -w $TOP/build/sign/testkey.x509.pem $TOP/build/sign/testkey.pk8 /tmp/$BUILDZIP $OUT/$GARCH/$BUILDZIP
+	if [ -x $(which md5sum) ]; then
+            echo "Creating md5sum..."
+            md5sum $OUT/$GARCH/$BUILDZIP >$OUT/$GARCH/$BUILDZIP.md5sum
         fi
-        if [ "$?" == 0 ]; then
-            return 0
-        else
-            return 1
-        fi
+    else
+        printerr "Couldn't zip files!"
+        return 1
+    fi
+    if [ "$?" == 0 ]; then
+        return 0
     else
         return 1
     fi
@@ -73,12 +75,16 @@ function zipit(){
 GARCH=$1
 create
 LASTRETURN=$?
-zipit
-LASTRETURN=$?
 if [ "$LASTRETURN" == 0 ]; then
-    printdone "Build completed: $OUT/$GARCH/$BUILDZIP"
-    exit 0
+    zipit
+    LASTRETURN=$?
+    if [ "$LASTRETURN" == 0 ]; then
+        printdone "Build completed: $OUT/$GARCH/$BUILDZIP"
+        exit 0
+    else
+        printerr "Build failed, check /tmp/gapps_log"
+        exit 1
+    fi
 else
-    printerr "Build failed, check /tmp/gapps_log"
     exit 1
 fi
